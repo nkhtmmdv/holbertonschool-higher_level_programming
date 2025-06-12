b7c2ce4442d638ae4b8100cdff817fb22b16636d
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-12 11:26:35 +0000

    7

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-word1 = "object-oriented programming"
-word2 = "with"
-word3 = "Python"
-print(word1 + " " + word2 + " " + word3)
+
+
+
+print("object-oriented programming with Python")
