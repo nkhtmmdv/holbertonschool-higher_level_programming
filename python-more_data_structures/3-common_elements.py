4624e1122333e69ca59d942a5e87b16538c807b8
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-18 11:37:58 +0000

    3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    unq_elem = []
+    
+    for elem in set_1:
+        if elem in set_2 and elem not in unq_elem:
+            unq_elem.append(elem)
+    
+    return unq_elem
