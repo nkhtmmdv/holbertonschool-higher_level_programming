54999778cb1becac060b4a1e34739f9788dd8a8f
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-19 10:37:00 +0000

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -7,5 +6,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-        print()
+    print()
     return count
