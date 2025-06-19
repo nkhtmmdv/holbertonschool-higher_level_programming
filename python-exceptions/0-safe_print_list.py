26fd470d7dc57405d37f09b2022c7febad5936ad
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-19 10:11:59 +0000

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+def count_elements(my_list=[]):
+    count1 = 0
+    for _ in my_list:
+        count1 += 1
+    return count1
+
 def safe_print_list(my_list=[], x=0):
-    count = 0  
+    count = 0
+    if x > count_elements(my_list):
+        x = count_elements(my_list)  
     for i in range(x):
         try:
             print(my_list[i], end="")  
-            count += 1  
+            count += 1
         except IndexError:
             break  
     print()  
