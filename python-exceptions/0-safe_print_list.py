fafa5a551ced5d10b3861f990265ed3a8a432005
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-19 10:55:56 +0000

    finally

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
+    try:
+        count = 0
+        for i in range(x):
             print(my_list[i], end="")
             count += 1
-        except IndexError:
-            break
-    print()
+        print()
+    except (IndexError, TypeError):
+        print()
     return count
