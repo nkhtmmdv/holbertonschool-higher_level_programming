aebcd09bf569b82812e2f2760effc0edb43677cc
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-27 10:15:48 +0000

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-''' 1 '''
+"""
+This module defines MyList class.
 
+Example:
+>>> my_list = MyList()
+>>> my_list.append(3)
+>>> my_list.append(1)
+>>> my_list.append(2)
+>>> print(my_list)
+[3, 1, 2]
+>>> my_list.print_sorted()
+[1, 2, 3]
+>>> print(my_list)
+[3, 1, 2]
+"""
 
 class MyList(list):
-    ''' 1 '''
+    """MyList class that inherits from list."""
+
     def print_sorted(self):
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
