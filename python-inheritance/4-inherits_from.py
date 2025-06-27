b43dbe7d287fe1ea0e8981451449fd7da1486825
nkhtmmdv <mamedoffnikh@gmail.com>
2025-06-27 11:19:26 +0000

    4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' 4 '''
+
+
+def inherits_from(obj, a_class):
+    ''' 4 '''
+    obj_type = type(obj)
+    if issubclass(obj_type, a_class) and obj_type is not a_class:
+        return True
+    return
