dac9fcfe047d56be700c23b7e81f3eb4f8ce01b0
nkhtmmdv <mamedoffnikh@gmail.com>
2025-07-12 10:26:34 +0000

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-''' 0 '''
+''' 0 ''' 
 
 
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
+def fetch_status():
+    """Fetch and display status content from a given URL."""
+    url = 'https://intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    ''' 0 '''
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+if __name__ == "__main__":
+    fetch_status()
+
