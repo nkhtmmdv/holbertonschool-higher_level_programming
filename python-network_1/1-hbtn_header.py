aeb082a20cb44723a10af7a3ab7416508e744a07
nkhtmmdv <mamedoffnikh@gmail.com>
2025-07-12 10:40:10 +0000

    1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """1"""
-
-
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
     """1"""
-    print(response.getheader('X-Request-Id'))
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        """1"""
+        print(response.getheader('X-Request-Id'))
