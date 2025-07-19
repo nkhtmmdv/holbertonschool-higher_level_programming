88455c9e6a84077789f153e272b51c5f449db3a1
nkhtmmdv <mamedoffnikh@gmail.com>
2025-07-19 11:29:19 +0000

    3

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -45,3 +45,6 @@ def display_products():
             products = []
 
     return render_template('product_display.html', products=products, error=error)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000)
